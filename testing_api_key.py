c04995a9bdcd374796ea435a6b396d5dd8bc45a0
Evan Banerjee <ebanerjee22@gmail.com>
2023-07-01 15:37:46 -0400

    copy and paste shit

--- testing_api_key.py
+++ testing_api_key.py
@@ -1,4 +1,33 @@
-print("hello world")
+import openai
 
-api_key = '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'
+API_KEY = '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'
+
+openai.api_key = API_KEY
+model_id = 'gpt-3.5-turbo-16k-0613'
+
+def chatgpt_conversation(conversation_log):
+    response = openai.ChatCompletion.create(
+        model=model_id,
+        messages=conversation_log
+    )
+
+    conversation_log.append({
+        'role': response.choices[0].message.role,
+        'content': response.choices[0].message.content.strip()
+    })
+    return conversation_log
+
+conversations = []
+# system, user, assistant
+conversations.append({'role': 'system', 'content': 'How may I help you?'})
+conversations = chatgpt_conversation(conversations)
+print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
+
+while True:
+    prompt = input('User: ')
+    conversations.append({'role': 'user', 'content': prompt})
+    conversations = chatgpt_conversation(conversations)
+    print()
+    print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
+
 
